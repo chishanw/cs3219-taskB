a6af02ff98882962721b7a59424be0c5bda71c28
wongchishan <wongchishan99@gmail.com>
2020-09-21 14:44:31 +0800

    Fix cors headers

--- handler.js
+++ handler.js
@@ -34,16 +34,18 @@ exports.new = function (event, context, callback) {
         .then(contact => {
             callback(null, {
                 statusCode: 200,
-                body: JSON.stringify(contact)
+                body: JSON.stringify(contact),
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Methods': 'OPTIONS,GET,POST'
+                }
             })
         })
         .catch(err => {
             callback(null, {
                 statusCode: err.statusCode || 500,
-                headers: { 'Content-Type': 'text/plain',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Headers': 'Content-Type',
-                    'Access-Control-Allow-Methods': 'OPTIONS,GET,POST' },
+                headers: { 'Content-Type': 'text/plain' },
                 body: 'Could not create the contact.'
             })
         })
@@ -57,16 +59,18 @@ exports.view = function(event, context, callback) {
         .then(contact => {
             callback(null, {
                 statusCode: 200,
-                body: JSON.stringify(contact)
+                body: JSON.stringify(contact),
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Methods': 'OPTIONS,GET,PUT,DELETE'
+                }
             })
         })
         .catch(err => {
             callback(null, {
                 statusCode: err.statusCode || 500,
-                headers: { 'Content-Type': 'text/plain',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Headers': 'Content-Type',
-                    'Access-Control-Allow-Methods': 'OPTIONS,GET,PUT,DELETE' },
+                headers: { 'Content-Type': 'text/plain' },
                 body: 'Could not fetch the contact.'
             })
         })
@@ -81,16 +85,18 @@ exports.update = function(event, context, callback) {
         .then(contact => {
             callback(null, {
                 statusCode: 200,
-                body: JSON.stringify(contact)
+                body: JSON.stringify(contact),
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Methods': 'OPTIONS,GET,PUT,DELETE'
+                }
             })
         })
         .catch(err => {
             callback(null, {
                 statusCode: err.statusCode || 500,
-                headers: { 'Content-Type': 'text/plain',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Headers': 'Content-Type',
-                    'Access-Control-Allow-Methods': 'OPTIONS,GET,PUT,DELETE' },
+                headers: { 'Content-Type': 'text/plain' },
                 body: 'Could not update the contact.'
             })
         })
@@ -107,16 +113,18 @@ exports.delete = function(event, context, callback) {
                 body: JSON.stringify({
                     message: 'Removed the contact!',
                     contact: contact
-                })
+                }),
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Methods': 'OPTIONS,GET,PUT,DELETE'
+                }
             })
         })
         .catch(err => {
             callback(null, {
                 statusCode: err.statusCode || 500,
-                headers: { 'Content-Type': 'text/plain',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Headers': 'Content-Type',
-                    'Access-Control-Allow-Methods': 'OPTIONS,GET,PUT,DELETE' },
+                headers: { 'Content-Type': 'text/plain' },
                 body: 'Could not delete the contact.'
             })
         })
