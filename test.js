bcdeb4e890c224d8d800e51c043d57fb73ad1cb4
wongchishan <wongchishan99@gmail.com>
2020-10-13 17:12:58 +0800

    Add PUT and DELETE test

--- test.js
+++ test.js
@@ -11,8 +11,9 @@ chai.use(chaiHttp);
 chai.should();
 
 describe("Contacts", () => {
+    var id;
     describe("POST /", () => {
-        it("should post one contact to database", (done) => {
+        it("should post Alice's contact to database", (done) => {
             chai.request(app)
                 .post('/api/contacts')
                 .send({
@@ -24,36 +25,51 @@ describe("Contacts", () => {
                 .end((err, res) => {
                     expect(err).to.be.null;
                     res.should.have.status(200);
-                    done();
-                });
-        });
-
-        it("should post one more contact to database", (done) => {
-            chai.request(app)
-                .post('/api/contacts')
-                .send({
-                    name: "Bob",
-                    email: "bob@gmail.com",
-                    phone: "91234568",
-                    gender: "Male"
-                })
-                .end((err, res) => {
-                    expect(err).to.be.null;
-                    res.should.have.status(200);
+                    res.body.data.should.be.a('object');
+                    id = res.body.data._id;
                     done();
                 });
         });
     });
 
+    describe("PUT /", () => {
+        it("should update Alice's email", done => {
+            chai.request(app)
+                .put('/api/contacts/' + id)
+                .send({
+                    email: "alice.tan@gmail.com"
+                })
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    res.should.have.status(200);
+                    res.body.data.should.be.a('object');
+                    done();
+                });
+        })
+    })
+
     describe("GET /", () => {
-        it("should get all contacts record", (done) => {
+        it("should get all contacts records", (done) => {
              chai.request(app)
                  .get('/api/contacts')
                  .end((err, res) => {
+                     expect(err).to.be.null;
                      res.should.have.status(200);
                      res.body.data.should.be.a('array');
                      done();
                   });
          });
     });
+
+    describe("DELETE /", () => {
+        it("should delete Alice's contact record", done => {
+            chai.request(app)
+                .delete('/api/contacts/' + id)
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    res.should.have.status(200);
+                    done();
+                })
+        })
+    })
 });
